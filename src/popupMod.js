ed96a378d176dafff80000a5ba836cf60f49df38
Nathan Laundry <nathan.laundry@gmail.com>
2023-09-27 07:43:47 -0400

    fixed accidental removal of prompt structuring

--- src/popupMod.js
+++ src/popupMod.js
@@ -19,7 +19,7 @@ export function initializePopup() {
 async function handleFormSubmit(event) {
     console.log("handle form submit")
     event.preventDefault(); // Prevent default form submission behavior
-    
+
     // Get form values
     const formValues = {
         transformationType: getSelectedRadioValue('transformationType'),
@@ -66,10 +66,23 @@ function getSelectedRadioValue(name) {
 function constructPrompt(formValues) {
     let prompt = '';
     const instructorIdentity = formValues.instructorIdentity || "professor of computer science";
-    const levelOfExpertise = formValues.levelOfExpertise || "high"; 
-    const areaOfInterest = formValues.areaOfInterest || "theoretical computer science"; 
-
-    // rest of the code remains the same...
-
+    const levelOfExpertise = formValues.levelOfExpertise || "high";
+    const areaOfInterest = formValues.areaOfInterest || "theoretical computer science";
+    switch (formValues.transformationType) {
+        case 'personalize':
+            prompt = `Transform the following text and maintain its length within -1/+1 paragraph. Assume it is educational material that you will now modify to be more personalized. Adjust the wording and examples, conceptual explanations and/or metaphors to be appropriate to the level of expertise provided which is ${levelOfExpertise}. Replace any explanations, usecases, examples, practice problems, or other components of the educational material to be appropriate for the area of interest which is ${areaOfInterest}. Phrase all wordings as if you were a ${instructorIdentity}. \n\n ${highlightedText}`;
+            break;
+        case 'simplify':
+            prompt = `As a ${instructorIdentity} with a level of expertise of ${levelOfExpertise} and an area of interest in ${areaOfInterest}, simplify the following text while keeping the core ideas intact. Make it more understandable for a beginner. \n\n ${highlightedText}`;
+            break;
+        case 'concreteExample':
+            prompt = `As a ${instructorIdentity} with a level of expertise of ${levelOfExpertise} and an area of interest in ${areaOfInterest}, provide a concrete example or real-life application of the concepts in the following text. \n\n ${highlightedText}`;
+            break;
+        case 'metaphoricalExplanation':
+            prompt = `As a ${instructorIdentity} with a level of expertise of ${levelOfExpertise} and an area of interest in ${areaOfInterest}, provide a metaphorical explanation of the ideas in the following text. Make it creative and engaging. \n\n ${highlightedText}`;
+            break;
+        default:
+            break;
+    }
     return prompt;
 }
