bb12df3bbf68f16650068e1b99d031306bbe224d
Jaeyong Lee <lee.j1533@gmail.com>
2023-11-11 21:31:20 -0500

    add background script messaging for install type

--- src/background.js
+++ src/background.js
@@ -1,3 +1,9 @@
+let extensionInstallType; 
+
+chrome.management.getSelf(myInfo =>{
+    extensionInstallType = myInfo.installType
+})
+
 // Send request to content script to find and replace on screen text
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message)
@@ -6,5 +12,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
             const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
             await chrome.tabs.sendMessage(tab.id, message = { req: 'run', body: message.body });
         })();
+    }
+});
+
+// Provide install type to content script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === 'get-install-type') {
+      sendResponse(extensionInstallType);
     }
 });
